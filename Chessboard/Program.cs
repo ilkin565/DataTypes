351bc875de78f652701334973919d3a7371cf3ba
ilkin <ilkin@PUPA>
2025-05-21 08:18:19 +0300

    Dz

---
+++ Chessboard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessboard
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите размер блока:");
+			int size;
+
+			while (!int.TryParse(Console.ReadLine(), out size) || size <= 0) ;
+
+			int repeatCount = 4;
+
+			for (int row = 0; row < repeatCount * size; row++)
+			{
+				for (int col = 0; col < repeatCount * size; col++)
+				{
+					bool isStarCell = ((row / size) + (col / size)) % 2 == 0;
+
+					if (isStarCell)
+						Console.Write("* ");
+					else
+						Console.Write("  ");
+				}
+				Console.WriteLine();
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Rhombus/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhombus
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите размер ромба:");
+			int size;
+			while (!int.TryParse(Console.ReadLine(), out size) || size <= 0) ;
+
+			int totalLines = 2 * size;
+
+			for (int i = 1; i <= totalLines; i++)
+			{
+				int leadingSpaces;
+				string line;
+
+				if (i <= size)
+				{
+					leadingSpaces = size - i;
+					line = new string(' ', leadingSpaces) + " /" + new string(' ', 2 * i - 2) + "\\";
+				}
+				else
+				{
+					leadingSpaces = i - size;
+					line = new string(' ', leadingSpaces) + "\\" + new string(' ', 2 * (totalLines - i) + 0) + "/";
+				}
+
+				Console.WriteLine(line);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
